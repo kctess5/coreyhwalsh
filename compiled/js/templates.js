e1fafca42253d109df54860f880ae99e6b52d43e
kctess5 <corey.walsh11@gmail.com>
2014-12-10 01:59:15 -0500

    The site is now responsive (yay)

--- compiled/js/templates.js
+++ compiled/js/templates.js
@@ -56,9 +56,9 @@ this["Templates"]["content"] = Handlebars.template({"1":function(depth0,helpers,
   if (stack1 != null) { buffer += stack1; }
   return buffer + "\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data,depths) {
-  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "<div class=\""
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "<div id=\"container\" class=\""
     + escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\n	<h2 class=\"title\">"
+    + "\">\n	<a id=\"menu-icon\">\n		<img src=\"/assets/menu27.png\">\n		<h3>Menu</h3><br>\n	</a>\n	<h2 class=\"title\">"
     + escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
     + "</h2>\n";
   stack1 = helpers['if'].call(depth0, (depth0 != null ? depth0.subheader : depth0), {"name":"if","hash":{},"fn":this.program(1, data, depths),"inverse":this.noop,"data":data});
@@ -96,10 +96,10 @@ this["Templates"]["sidebar"] = Handlebars.template({"1":function(depth0,helpers,
   if (stack1 != null) { buffer += stack1; }
   return buffer + "</a></h3>\n			</div>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, buffer = "<div>\n	<h1 class=\"title\">";
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, buffer = "<div>\n	<h1 class=\"main title\"><a href=\"/\">";
   stack1 = ((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper));
   if (stack1 != null) { buffer += stack1; }
-  buffer += "</h1>\n	<div id=\"top-links\">\n";
+  buffer += "</a></h1>\n	<div id=\"top-links\">\n";
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.toplinks : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
   buffer += "	</div>\n	\n	<div id=\"bottom-links\">\n";
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
-console.log("main");
-
 var helpers = require("./helpers");
 var sidebar = helpers.compileTemplate("sidebar");
 var hash = window.location.hash.substr(1).split("/");
+
+function OpenInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
 
 var updateSidebar = function(id) {
 	$( "#sidebar .link" ).removeClass("active")
@@ -34,13 +37,11 @@ var loadDetails = function(pageId, articleId, template) {
 		return el.id == articleId;
 	})[0];
 
-	console.log(article);
-
 	if (template && article) {
 		var html = template(article);
-		// console.log(html)
 		replaceAndScroll("#content .detailed-content", html)
 	}
+	resizeContent()
 }
 
 var hideDetails = function() {
@@ -48,22 +49,76 @@ var hideDetails = function() {
 	$("#content .content").css("width", "100%");
 }
 
+var smallWidth = function(e) {
+	if ($("#content .content").outerWidth() < 125) {
+		console.log("hide")
+		e.stopPropagation(); 
+		e.preventDefault(); 
+		hideDetails();
+		return true;
+	}
+}
+
+var reset = function(){
+	console.log("reset")
+	$("body").css("padding-left", "");
+}
+
 var bind = function() {
-	$(".article.detailed").click(function() {
+	$(".article.detailed").click(function(e) {
+		if (smallWidth(e)) return;
 		var pageId = $(this).parent("div").attr("class");
 		var articleId = $(this).attr("id");
 		window.location.hash = "#"+pageId+"/"+articleId;
+		if ($(window).width() < 745) {
+			minimizeSidebar(true);
+		}
 		loadDetails(pageId, articleId, "details")
 	});
 	$(".article.linked").click(function(e) {
+		if (smallWidth(e)) return;
 		var href = $(e.target).attr("href");
 		if (!href) { // didn't click on link
 			var url = $(this).find("a").attr("href")
 			OpenInNewTab(url);
 		}
 	});
+	$("#menu-icon").click(function(e){
+		$("#sidebar").toggleClass("active");
+		var isActive = $("#sidebar").hasClass("active");
+		$("body").css("padding-left", isActive ? "180px" : "");
+	});
+	$("#content .content").click( function( e ) {
+		var parent = $(e.target).parents('.detailed').get()[0];
+		var isArticle = $(e.target).is(".detailed");
+
+		if ( parent == undefined && !isArticle ) hideDetails();
+	});
+
 }
 
+var resizeContent = function() {
+	var windowWidth = $(window).width();
+	var width = $("#content .content").css("width");
+	var detailed = $(".detailed-content");
+	if (detailed.is(":visible")) {
+		if (windowWidth < 560) {
+			var target = windowWidth - detailed.outerWidth();
+			$("#content .content").css("width", target);
+		} else {
+			$("#content .content").css("width", "50%");
+		}
+	}
+	if (windowWidth > 744) {
+		reset();
+	}
+}
+
+var minimizeSidebar = function(cond) {
+	$("body").css("padding-left", cond ? "0px" : "");
+	$("#sidebar").removeClass("active");
+	if (cond) $("#sidebar").fadeOut(100);
+}
 
 $("#sidebar").append(sidebar)
 if (hash[0]) swapFor(hash[0], "content");
@@ -71,18 +126,17 @@ else swapFor("about", "content"); // default to about page
 
 if (hash[1]) loadDetails(hash[0],hash[1], "details");
 bind();
-
+resizeContent();
 
 $(".link").click(function() {
 	var id = $(this).attr("id");
+	console.log($("#sidebar").hasClass("active"),  $(window).width() < 460)
+	if ($("#sidebar").hasClass("active") && $(window).width() < 460) {
+		minimizeSidebar();
+	}
 	hideDetails();
 	swapFor(id, "content")
 	bind();
 })
 
-function OpenInNewTab(url) {
-  var win = window.open(url, '_blank');
-  win.focus();
-}
-
-
+$( window ).resize(resizeContent);
